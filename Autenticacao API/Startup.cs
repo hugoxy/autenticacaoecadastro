25a311577e4ffdd5e33045c80363ac223e34a4e6
Victor <xhugo@live.com>
2022-05-24 21:00:39 -0300

    habilitar CORS

--- Autenticacao API/Startup.cs
+++ Autenticacao API/Startup.cs
@@ -88,12 +88,17 @@ namespace Autenticacao_API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseCors(options => options
+              .AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+
             //if (env.IsDevelopment())
-            //{
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Autenticacao_API v1"));
-            //}
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Autenticacao_API v1"));
+           
 
             app.UseHttpsRedirection();
 
@@ -106,6 +111,8 @@ namespace Autenticacao_API
             {
                 endpoints.MapControllers();
             });
+
+         
         }
     }
 }
